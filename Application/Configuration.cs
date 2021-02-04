e3e6407e7f2c142723450f4e74b58862ea4adce9
kovalevgr <kovalevgr@gmail.com>
2021-02-04 14:42:59 +0200

    init project

---
+++ Application/Configuration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DocHelper.Application
+{
+    public static class Configuration
+    {
+        public static IServiceCollection ConfigureApplication(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services;
+        }
+    }
+}
---
+++ Infrastructure/Configuration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DocHelper.Infrastructure
+{
+    public static class Configuration
+    {
+        public static IServiceCollection ConfigureInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services;
+        }
+    }
+}
