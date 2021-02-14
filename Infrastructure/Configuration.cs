62139df619c258821c51e9eec1c9636d585d42fb
kovalevgr <kovalevgr@gmail.com>
2021-02-14 14:20:09 +0200

    Add EF + DBContext

--- Infrastructure/Configuration.cs
+++ Infrastructure/Configuration.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using DocHelper.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace DocHelper.Infrastructure
@@ -7,6 +9,11 @@ namespace DocHelper.Infrastructure
     {
         public static IServiceCollection ConfigureInfrastructure(this IServiceCollection services, IConfiguration configuration)
         {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    configuration.GetConnectionString("DefaultConnection"),
+                    b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+            
             return services;
         }
     }
