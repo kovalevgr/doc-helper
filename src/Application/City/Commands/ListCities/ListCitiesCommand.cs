5b726e7714bda988070b0e048af6f25473df247b
kovalevgr <kovalevgr@gmail.com>
2021-02-28 18:33:27 +0200

    Add sites Command

---
+++ src/Application/City/Commands/ListCities/ListCitiesCommand.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using DocHelper.Application.Common.Interfaces;
+using MediatR;
+
+namespace DocHelper.Application.City.Commands.ListCities
+{
+    public class ListCitiesCommand : IRequest<List<Domain.Entities.City>>
+    { }
+
+    public class ListCitiesCommandHandler : IRequestHandler<ListCitiesCommand, List<Domain.Entities.City>>
+    {
+        private readonly IApplicationDbContext _applicationDbContext;
+
+        public ListCitiesCommandHandler(IApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
+        public async Task<List<Domain.Entities.City>> Handle(ListCitiesCommand request,
+            CancellationToken cancellationToken)
+        {
+            return _applicationDbContext.Cities.ToList();
+        }
+    }
+}
