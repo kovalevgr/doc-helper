432f8d323581f25cbd372f501358789923020afe
kovalevgr <kovalevgr@gmail.com>
2021-05-20 23:29:47 +0300

    Fix

--- src/CabinetApp/Program.cs
+++ src/CabinetApp/Program.cs
@@ -1,10 +1,14 @@
 using System;
 using System.Net.Http;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Text;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
-namespace DocHelper.CabinetApp
+namespace CabinetApp
 {
     public class Program
     {
