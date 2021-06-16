c373d0e5f68d9273b7ae5477e54a77bd3d1e7890
kovalevgr <kovalevgr@gmail.com>
2021-06-16 14:32:44 +0300

    Add Create doctor page

---
+++ src/CabinetApp/Components/Doctor/CreateDoctorComponent.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using Doctor;
+using JetBrains.Annotations;
+using Microsoft.AspNetCore.Components;
+using Channel = Grpc.Net.Client.GrpcChannel;
+
+namespace CabinetApp.Components.Doctor
+{
+    public class CreateDoctorComponent : ComponentBase
+    {
+        [CanBeNull] private DoctorData Doctor;
+        private Channel ChannelBase { get; }
+
+        protected async Task Create()
+        {
+            var client = new DoctorRPCService.DoctorRPCServiceClient(ChannelBase);
+
+            var response = await client.CreateDoctorAsync(Doctor);
+        }
+    }
+}
