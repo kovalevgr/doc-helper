0888b307106c34ed21154eb300c25e8c174868c5
kovalevgr <kovalevgr@gmail.com>
2021-06-24 21:58:35 +0300

    Add Kafka message broker

--- src/Infrastructure/MessageBrokers/Kafka/KafkaListener.cs
+++ src/Infrastructure/MessageBrokers/Kafka/KafkaListener.cs
@@ -1,29 +1,72 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Confluent.Kafka;
 using DocHelper.Domain.Events;
+using DocHelper.Infrastructure.Events;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
 
 namespace DocHelper.Infrastructure.MessageBrokers.Kafka
 {
     public class KafkaListener : IEventListener
     {
+        private readonly IEventBus _eventBus;
+        private readonly KafkaOptions _options;
+
+        private string[] Topics => _options.Topics.Split(";");
+
+        public KafkaListener(IEventBus eventBus, IOptions<KafkaOptions> options)
+        {
+            _eventBus = eventBus;
+            _options = options.Value;
+        }
+
+        public void Subscribe<TEvent>() where TEvent : IEvent => Subscribe(typeof(TEvent));
+
         public void Subscribe(Type type)
         {
-            throw new NotImplementedException();
+            using var consumer = new ConsumerBuilder<string, string>(_options.Consumer).Build();
+            consumer.Subscribe(Topics);
+            while (true)
+            {
+                var message = consumer.Consume();
+
+                var @event = JsonConvert.DeserializeObject(message.Message.Value, type) as IEvent;
+
+                _eventBus.PublishLocal(@event);
+            }
         }
 
-        public void Subscribe<TEvent>() where TEvent : IEvent
+        public async Task Publish<TEvent>(TEvent @event) where TEvent : IEvent
         {
-            throw new NotImplementedException();
+            using var p = new ProducerBuilder<string, string>(_options.Producer).Build();
+            await p.ProduceAsync(_options.Topic,
+                new Message<string, string>
+                {
+                    Key = MessageBrokersHelper.GetTypeName<TEvent>(),
+                    Value = JsonConvert.SerializeObject(@event)
+                });
         }
 
-        public Task Publish<TEvent>(TEvent @event) where TEvent : IEvent
+        public async Task Publish(string message, string type)
         {
-            throw new NotImplementedException();
-        }
+            if (string.IsNullOrWhiteSpace(message))
+            {
+                throw new ArgumentNullException(nameof(message), "Event message can not be null.");
+            }
 
-        public Task Publish(string message, string type)
-        {
-            throw new NotImplementedException();
+            if (string.IsNullOrWhiteSpace(type))
+            {
+                throw new ArgumentNullException(nameof(type), "Event type can not be null.");
+            }
+
+            using var p = new ProducerBuilder<string, string>(_options.Producer).Build();
+            await p.ProduceAsync(_options.Topic,
+                new Message<string, string>
+                {
+                    Key = type,
+                    Value = message
+                });
         }
     }
 }
