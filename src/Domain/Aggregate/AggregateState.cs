63fade4bb68a51f6085b02c69c2ac83d0331fe63
kovalevgr <kovalevgr@gmail.com>
2021-06-28 23:53:55 +0300

    Add aggregation state

--- src/Domain/Aggregate/AggregateState.cs
+++ src/Domain/Aggregate/AggregateState.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using DocHelper.Domain.Events;
 
-namespace DocHelper.Domain.Repository
+namespace DocHelper.Domain.Aggregate
 {
     public class AggregateState
     {
@@ -10,6 +10,12 @@ namespace DocHelper.Domain.Repository
         public int Version { get; } = 0;
         public DateTime CreatedUtc { get; }
         public IEnumerable<IEvent> Events { get; } = new List<IEvent>();
+        
+        public AggregateState(Guid id, IEnumerable<IEvent> events)
+        {
+            Id = id;
+            Events = events;
+        }
         
         public AggregateState(Guid id, int version, DateTime createdUtc)
         {
--- src/Infrastructure/Repository/AggregateRepository.cs
+++ src/Infrastructure/Repository/AggregateRepository.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using DocHelper.Domain.Aggregate;
 using DocHelper.Domain.Entities;
 using DocHelper.Domain.Events;
 using DocHelper.Domain.Repository;
+using DocHelper.Infrastructure.EventStores;
 using DocHelper.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore.Storage;
 using static System.GC;
@@ -15,12 +17,19 @@ namespace DocHelper.Infrastructure.Repository
     public abstract class AggregateRepository<T> : BaseRepository<T>, IAggregateRepository
         where T : BaseEntity
     {
+        private readonly IEventStore _eventStore;
+
         private readonly List<IEvent> _uncommittedEvents = new();
         private IDbContextTransaction _transaction;
-        private bool _disposed = false;
+        private bool _disposed;
 
-        protected AggregateRepository(ApplicationDbContext dbContext) : base(dbContext)
-        { }
+        protected Guid AggregateId { get; } = default;
+
+        protected AggregateRepository(ApplicationDbContext dbContext, IEventStore eventStore)
+            : base(dbContext)
+        {
+            _eventStore = eventStore;
+        }
 
         public async void BeginTransaction()
         {
@@ -30,6 +39,8 @@ namespace DocHelper.Infrastructure.Repository
         public async void SaveChanges()
         {
             await DbContext.SaveChangesAsync();
+
+            await _eventStore.Store(GetState());
         }
 
         public async void Commit()
@@ -41,7 +52,7 @@ namespace DocHelper.Infrastructure.Repository
         {
             await _transaction.RollbackAsync();
         }
-        
+
         protected override async Task<TEntity> AddAsync<TEntity>(TEntity entity,
             CancellationToken cancellationToken = default)
         {
@@ -62,6 +73,11 @@ namespace DocHelper.Infrastructure.Repository
         protected virtual void Enqueue(IEvent @event)
         {
             _uncommittedEvents.Add(@event);
+        }
+
+        protected virtual AggregateState GetState()
+        {
+            return new(AggregateId, _uncommittedEvents);
         }
 
         protected virtual void Dispose(bool disposing)
